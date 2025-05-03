863c4d6cc1baf507b94bf243859ab3b9bce6f243
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-05-04 03:11:10 +0530

    added new topic

--- javascript/day09.js
+++ javascript/day09.js
@@ -32,16 +32,65 @@
 // console.log(now);// yeah bhi hume same Gettime ki trah time access krke dete hai milliseocnd me 
 // GetTime se date.now tak aane me kuch too time lagega , isislye millisecond me variation ata hai
 
-const d =new Date("2025-05-03T10:05:20");// string ke andar jab hum koe date dete hai tab woo one se access hoti hai..uski date and day
+// const d =new Date("2025-05-03T10:05:20");
+// year / month / date /Hour / minute / second / millisecond
+
+// string ke andar jab hum koe date dete hai tab woo one se access hoti hai..uski date and day
 // yaha pe hum alag se time bhi add kr skte hai
-console.log(d);
-console.log(d.toDateString());// humne yaha pe date ko string ke andr store krke rakha hai ,so start one se ho rha hai ,isilye output me may aa rha hai , agar zeo se start hua hota too , yeah april show krta 
-const date =new Date(2025,4,26)// yaha pe data number ki form me hai too zero based indexing ho rha hai aur indian standard ke according time batatega
-console.log(date.toString());
+// console.log(d);
+// console.log(d.toDateString());// humne yaha pe date ko string ke andr store krke rakha hai ,so start one se ho rha hai ,isilye output me may aa rha hai , agar zeo se start hua hota too , yeah april show krta 
+// const date =new Date(2025,4,26)// yaha pe data number ki form me hai too zero based indexing ho rha hai aur indian standard ke according time batatega
+// console.log(date.toString());
+
+// Number : 0 based start honge
+// string : 1 based start honge
+
+// SETTING DATE COMPONENTS //simply hum yaha pe date ka setting krege
+
+// const d = new Date();
+// d.setDate(21);
+// d.setFullYear(2025);
+// d.setMonth(3);
+// console.log(d);
+// console.log(d.toString());
 
 
+// DATE CALCULATION
+
+// const date1 = new Date();// yaha pe date system se lega
+// const date2 = new Date("2025-09-21");// yaha pe humne khudse input provide kiya hai 
+// console.log(date2-date1);// output iska millisecond me aaega
+// // difference between dates comes in millisecond
+// console.log(date2>date1);// output true
+// console.log(date2<date1);// output false
+
+// COUNTDOWN TIMER 
+// we are making countdown timer for the olympics
+
+// days ,hours ,minute ,second
+
+const date1 = new Date(); // system se date access kr rha hai
+const date2 = new Date("2028-07-14T00:00:00");// yaha pe humne khudse date input diya hai
+
+const date = date2-date1;
+// const days1 = Math.floor(date/(1000*60*60*24));// yaha pe humne floor fn ke help se decimal ki phele tak ki value access ki hai 
+const days = date/(1000*60*60*24);// millisec to sec to hrs to days
+// console.log(days);
+// console.log(days1);
+// hour calculation keliye below
+const hour = Math.floor((date/(1000*60*60))%24);// yaha pe humne kyu moudulus 24 liya hai doubt ??kyuki modules use krne ke baad hum days to find out kr lete hai then woo hume remaning time bhi inform bhi kr deta hai
+// console.log(hour);
+
+// minute calculation keliye
+
+const minute = Math.floor((date/(1000*60))%60);// minute me calculate kr rhe hai
+// console.log(minute);
+
+const second =Math.floor((date/(1000))%60);
+// console.log(second);
 
 
+console.log(`Olympics CountDownTime: Days:${days} hour:${hour} minute:${minute} second:${second}`);// i used backtick to acces the dollar sign work
 
 
 
