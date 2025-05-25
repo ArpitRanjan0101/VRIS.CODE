c7333399dab5651e9e60ef22b560ed30a1d6edf0
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-05-25 15:18:12 +0530

    FEAT:update in code

---
+++ javascript/day12.js
@@ -0,0 +1,76 @@
+// -------------Function-------------
+
+// function greet(){ // function defining
+//     console.log("hello arpit ranjan");
+//     console.log("my frnd name is kajal");
+//     console.log("we are working together since 5 years");
+// }
+// greet();     // this is call statement of the fn
+// greet();      // by using this we can call the stat into the fn multpiple times just calling to it.
+
+// ---------Above is the normal function program
+
+
+// --------------- ADD two numbers program-----------
+
+// function sum(num1,num2){   // fn ka jo name usse hi call krna hota hai speeling bhi same hona chye and it contain the parameter
+//     console.log(num1+num2);
+// };
+
+// sum(3,4); // argument
+
+// -------------Multiplication :using return----------- 
+
+// function multiply(num1,num2){
+//     return num1*num2;
+// }
+// console.log(multiply(3,5));
+
+
+// ----------2nd way to call below--------------
+
+// function multiply(num1,num2){
+//     return(num1*num2);
+// }
+
+// let result=multiply(4,5);
+// console.log(result);
+
+
+///--------- WE can store function in variable too--------
+
+// const fun=function(){
+//     console.log("hello world");
+//     console.log("my name is arpit ranjan");
+// }
+// fun();
+
+
+//------------- ONe more thing too be noted-----------
+
+// const fun =function(){
+//     console.log("hello everyone");
+//     console.log("kaise ho applog");
+//     return "rukjao na";
+//     // console.log("mai khuss hu tum kaise ho  bro"); // yeah line kabhi execute nhi hoga..
+// }
+
+// console.log(fun());// in this statment first each console will be printed then return will be printed..return ke andr agar koe console likhte hai too wo return nhi hoga note this one.
+
+
+
+//----------------Fat Arrow function
+// same function it will perform but it make the code short and crispy and easier 
+
+const fun=()=>{
+    console.log("hello coder army"); 
+}
+
+
+
+
+
+
+
+
+
