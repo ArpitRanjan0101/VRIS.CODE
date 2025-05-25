d9c0ae6922400a38d48d817bb37698b1fbd39e7e
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-05-26 02:54:22 +0530

    FEAT:update in code

--- javascript/day13.js
+++ javascript/day13.js
@@ -1,3 +1,95 @@
 // ---------------WE WILL SEE HERE THE IF-ELSE AND LOOP IN JAVASCRIPT-------------
 
 
+// if -else
+// let age=30;
+
+// if(age>=18){
+//     console.log("eligible for vote");
+// }
+// else{
+//     console.log("Not eligible to vote");
+// }
+
+
+//--------------if elseif else
+
+// let age =19;
+
+// if(age<18){
+//     console.log("KID");
+// }
+// else if(age>45){
+//     console.log("OLD");
+// }
+// else {
+//     console.log("ADULT");
+// }
+
+
+//___________-----FOR MUITIPLE CONDITION------------
+
+//    let num=6;     // input taken from the from user
+//    switch(num)            // switch(new Date().getDay())
+// {
+//     case 0:
+//         console.log('SUNDAY');
+//         break;
+//     case 1:
+//         console.log('MONDAY');
+//         break;
+//     case 2:
+//         console.log('TUESDAY');
+//         break;
+//     case 3:
+//         console.log('WEDNESDAY');
+//         break;
+//     case 4:
+//         console.log('THURSDAY');
+//         break;
+//     case 5:
+//         console.log('FRIDAY');
+//         break;
+//     case 6:
+//         console.log('SATURDAY');
+//         break;
+//     case 7:
+//     console.log('SUNDAY');
+//     break;
+//     default:
+//     console.log('not the day');
+// }
+
+// for(let i=0;i<20;i++){ // we can take const in place of let , since it will become the constant value that will not change
+//     console.log("hello arpit how are you");
+// }
+
+
+// -------------- SUM OF NUMBERs---------------------
+
+// let sum=0;
+// for(let i=0;i<=10;i++){
+//     sum=sum+i;
+// }
+//  console.log(sum);
+
+
+// -----------NESTED FOR LOOP--------------
+
+//12345
+//12345
+//12345
+//12345
+//12345
+// for(let i=1;i<=5;i++){
+//     for(let j=1;j<=5;j++)
+//     {
+//         console.log(j);
+//     }
+// }
+
+
+// -------------------SCOPE IN JAVASCRIPT--------------------
+
+
+ 
