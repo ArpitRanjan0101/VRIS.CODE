07bc298f4f15654ba6389f133619bde4767786be
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-05-29 03:49:44 +0530

    working on typescript

---
+++ typescript/first.js
@@ -0,0 +1,39 @@
+ let balance=200;
+ balance="three hundred";
+ console.log(balance*10); // output will be NAN
+
+let age =20;
+age ='twenty';
+console.log(age); // here we try to fetch the data from backend , wanna print 20 number , as we updated the age with string number the javascript printed the string number this is the problem in js
+
+let obj={
+    name:'arpit',
+    age:17
+}
+console.log(obj.height);// here we no keys related to the height yet in console it gave output undefined ,but it should give error in output in js, causing problem in debuging
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
