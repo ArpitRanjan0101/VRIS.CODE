1ea8af058160b3350e062c0ad57f195506336c86
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-05-30 23:00:36 +0530

    FEAT: finish up tsc d2

--- typescript/day2.js
+++ typescript/day2.js
@@ -3,6 +3,64 @@ let num = 10;
 num = "arpit";
 let x = 10;
 let val = "rohit";
-let money; // whenever we define the variable without assigning any value to it the , tsc define its type as any ,mean we can assign any value to it.
+//---------------------------- 
+let money; // whenever we define the variable without assigning any value to it then , tsc define its type as any ,mean we can assign any value to it, then it become dengerour situation , we have to keep away from this situation.
+//--------------------------
 money = 20;
 money = "rohit";
+//------------------
+let val2; // agar unknown type ki aagyi to kaise tackle krege..
+val2 = 20;
+val2 = "Rohit";
+//-----------------------------
+let num = 10;
+let x = 10;
+let val = "Rohit";
+let money;
+money = "Rohit";
+money = 20;
+// console.log(money.toUpperCase());
+let val2;
+val2 = "Rohit";
+val2 = 10;
+if (typeof val2 === 'string')
+    console.log(val2.toUpperCase());
+if (typeof val2 === 'number')
+    console.log(val2.toFixed(2));
+// Non Primitive
+let arr = [2, 4, 5, 7, 11];
+let arr2 = [2, 1, 19, 10];
+let arr3 = ["Rohit", 20, 11, "Sohan"];
+arr3.push(10);
+let arr4 = ["rohit", 10, false, 11];
+let tuple = ["Rohit", 10, 11];
+// Objects
+// inline
+let obj1 = {
+    name: "Rohit",
+    age: 20,
+    gender: "female"
+};
+let person;
+person = {
+    name: "rohit",
+    age: 20,
+    balance: 420
+};
+let n1;
+n1 = 10;
+let c1 = {
+    name: "Rohit",
+    age: 20,
+    id: "fshfsd"
+};
+;
+let obj3 = {
+    names: "Rohit",
+    age: 20,
+    position: "manager",
+    id: 210
+};
+// Interface vs Type
+// Function
+// Classes
