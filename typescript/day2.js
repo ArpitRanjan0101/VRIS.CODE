8253c90a43978761a11eb7b193446cdae8b9c003
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-05-30 22:56:59 +0530

    README update

--- typescript/day2.js
+++ typescript/day2.js
@@ -1,3 +1,8 @@
 "use strict";
 let num = 10;
 num = "arpit";
+let x = 10;
+let val = "rohit";
+let money; // whenever we define the variable without assigning any value to it the , tsc define its type as any ,mean we can assign any value to it.
+money = 20;
+money = "rohit";
--- typescript/day2.ts
+++ typescript/day2.ts
@@ -1,2 +1,20 @@
 let num:number=10
  num="arpit" 
+
+let x=10;
+let val ="rohit"
+//---------------------------- 
+
+let money; // whenever we define the variable without assigning any value to it then , tsc define its type as any ,mean we can assign any value to it, then it become dengerour situation , we have to keep away from this situation.
+
+//--------------------------
+
+money=20;
+money="rohit";
+
+//------------------
+
+let val2:unknown; // agar unknown type ki aagyi to kaise tackle krege..
+ val2=20;
+val2="Rohit";
+
