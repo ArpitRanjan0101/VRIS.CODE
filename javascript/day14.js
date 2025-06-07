587f14c149c9ac37d724ce47cd373e850e4714d6
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-06-08 02:35:41 +0530

    day14 update in code

---
+++ javascript/day14.js
@@ -0,0 +1,21 @@
+// ADVANCE LOOP
+
+let obj={
+    name:"Arpit",
+    age:23,
+    gender:"male",
+    city:"hazaribagh"
+};
+
+// for in loop , here we can iterate over index that is keys
+
+// for(let key in obj)
+// {
+//     console.log(key,obj[key]); // here we print the keys with its values by using  obj[key]
+// }
+
+// // Object.Keys(obj) yaha pe bhi hum iterate kr skte hai jo ki ek array return krega 
+// console.log(Object.keys(obj)); // alert capital o lena hai object me
+
+
+
