e879efa596dc22980c18a2fdcb5d592460c17362
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-06-26 15:56:21 +0530

    update

--- javascript/day14/first.js
+++ javascript/day14/first.js
@@ -5,5 +5,9 @@ obj.name="Arpit"; // yaha print hue key with it's value
 // yaha pe obj ke pass key value alwa writable ,enumerable and configurable properties hoti hai
 
 // console.log(obj); // print krne pe empty aa rhi hai
+console.log(Object.getOwnPropertyDescriptor(obj,'name'));
+obj.name ="Heer"
 
+// yaha pe humne dekha ki writable ,enumerable and configurable likha hua nhi tha yet we get it at output
  
+// writable = false agar ata hai too , value ko mai change nhi kar skta hu
