636190ca186373570250548a5beafb843cf865d7
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-06-28 00:33:37 +0530

    update

--- javascript/day14/first.js
+++ javascript/day14/first.js
@@ -58,3 +58,22 @@
 // obj1.account_number=20001;
 // console.log(obj1.account_number);
  
+ 
+//  
+
+const customer={
+    name:"Heer",
+    age:22,
+    account_number:123,
+    balance:2000,
+}
+
+// name , account_num , change nhi honi chye
+
+Object.defineProperty(customer,"name",{
+    writable:false,
+})
+
+customer.name="arpit";
+// customer.account_number=10001;
+console.log(customer);
