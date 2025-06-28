804df6f59ae3ca78feb0bc0adc82cc0eeb27438a
ArpitRanjan0101 <arpitvijans1234@gmail.com>
2025-06-28 23:48:54 +0530

    update in code

--- javascript/day15/callbackfn.js
+++ javascript/day15/callbackfn.js
@@ -1,12 +1,59 @@
 // call back function
 
-function names(){
-    console.log("hello i am arpit");
+// function names(){
+//     console.log("hello i am arpit");
+// }
+
+// function greet(){
+//     console.log("i am call back function");
+// }
+
+// names();
+// greet();  
+
+
+// FIRST WAY FOR CALL BACK FUNCTION
+
+// function names(fun){
+//     console.log("hello i am arpit");
+//     fun();
+// }
+
+// function greet(){
+//     console.log("i am call back function");
+// }
+
+// names(greet);   // function calling ke andr ki dusre function call ko pass kr diye as a reference
+
+
+
+// SECOND WAY FOR CALL BACK FUNCTION
+
+
+// function names(fun){
+//     console.log("hello i am arpit");
+//     fun();
+// }
+
+// names(function greet(){
+//     console.log("i am call back function");
+// })    // second way of function call me hum khud function calling ke andr hi dusre function ko define kr skte hai.
+
+
+// // Hum as a fat arrow function hi call kr skte hai
+
+// names(()=>{
+//     console.log("I am call back function");
+// });
+
+
+
+// DATA agar backend se fecth kr rhe hai too
+
+function fetchData(){    // Backend se data fetch krega aur yaha  pe show kr dega
+    // bhut sara code hog yaha pe
+    console.log("i am fetching data");
 }
 
-function greet(){
-    console.log("i am call back function");
-}
 
-names();
-greet();  
+setInterval(fetchData,5000);
